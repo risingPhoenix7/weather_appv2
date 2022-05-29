a650a2b6e80f37d1d70ee4d3d49ef04190292b68
risingPhoenix7 <achinthyahebbar@gmail.com>
2022-05-29 13:21:35 +0530

    full final!!!

--- lib/viewmodel/useful_data.dart
+++ lib/viewmodel/useful_data.dart
@@ -1,12 +1,13 @@
 import '../model/hourly_data_class.dart';
 
 class UsefulData {
-  static late Current? current;
-  static late  Current? second;
-  static late Current? third;
-static Current? requiredData(int a){
+  static late HourlyData? current;
+  static late  HourlyData? second;
+  static late HourlyData? third;
+static HourlyData? requiredData(int a){
   if(a==0) return current;
   if(a==1) return second;
   if(a==2) return third;
+  return null;
 }
 }
